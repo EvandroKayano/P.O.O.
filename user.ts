558d20f3792c82eb6c33c3c2429853a1d0ef144f
EvandroKayano <143224048+EvandroKayano@users.noreply.github.com>
2023-09-19 21:06:37 -0300

    Delete user.ts

--- user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    constructor(
-        public name: string,
-        public email: string,
-        public password: string,
-        // '?' significa que é um dado OPCIONAL
-        public id?: string
-    ){}
-}
