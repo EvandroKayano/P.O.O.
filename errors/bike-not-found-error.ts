ba6429bf90d4e7df24159ce509e68fe777935517
EvandroKayano <143224048+EvandroKayano@users.noreply.github.com>
2023-09-24 16:36:34 -0300

    Delete errors directory

--- errors/bike-not-found-error.ts
+++
@@ -1,7 +0,0 @@
-export class BikeNotFoundError extends Error{
-    public readonly = 'BikeNotFoundError'
-
-    constructor(){
-        super('Bike not found.')
-    }
-}
--- errors/duplicated-user-error.ts
+++
@@ -1,8 +0,0 @@
-
-export class DuplicatedUserError extends Error{
-    public readonly = 'DuplicatedUserError'
-
-    constructor(){
-        super('User duplicated.')
-    }
-}
